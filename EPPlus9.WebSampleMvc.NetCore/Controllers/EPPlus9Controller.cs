cb3e0862f983cbdb98070e89cadb2bbe6315076d
Jan Källman <jan.kallman@epplussoftware.com>
2026-09-10 09:56:37 +0200

    Started work on EPPlus 9 sample

---
+++ EPPlus9.WebSampleMvc.NetCore/Controllers/EPPlus9Controller.cs
@@ -0,0 +1,51 @@
+﻿using EPPlus9.WebSampleMvc.NetCore.Models.EPPlus9;
+using Microsoft.AspNetCore.Mvc;
+using OfficeOpenXml.Drawing.Chart.Style;
+using System;
+using System.Threading.Tasks;
+
+namespace EPPlus9.WebSampleMvc.NetCore.Controllers
+{
+    public class EPPlus9Controller : Controller
+    {
+        private const string ContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public async Task<IActionResult> HtmlExportwithCharts(HtmlExportWithChartModel model)
+        {
+            await model.LoadHtmlAndChartExport(model.ChartStyle, model.TableStyle);            
+            return View(model);
+        }
+        public async Task<IActionResult> HtmlExportDifferentCharts(HtmlExportDifferentChartsModel model, string action)
+        {
+            if(action=="download")
+            {
+                return File(HtmlExportDifferentChartsModel.CreateWorkbook(model.ChartType, model.ChartStyle, model.TableStyle).GetAsByteArray(), ContentType);
+            }
+            else
+            {
+                await model.LoadHtmlAndChartExport(model.ChartType, model.ChartStyle, model.TableStyle);
+                return View(model);
+            }
+        }
+        public async Task<IActionResult> DownloadWorkbook(HtmlExportWithChartModel model)
+        {
+            return File(HtmlExportWithChartModel.CreateWorkbook(model.ChartStyle, model.TableStyle).GetAsByteArray(), ContentType);
+        }
+        public async Task<IActionResult> DownloadDifferentChartWorkbook(HtmlExportDifferentChartsModel model)
+        {
+            return File(HtmlExportDifferentChartsModel.CreateWorkbook(model.ChartType, model.ChartStyle, model.TableStyle).GetAsByteArray(), ContentType);
+        }
+        public IActionResult DrawingsAsSvg()
+        {
+            return View();
+        }
+        public IActionResult PdfExport()
+        {
+            return View();
+        }
+    }
+}

a7e6ba5adf106e3c94f8e4c196557a1281cc3cb2
swmal <897655+swmal@users.noreply.github.com>
2026-09-16 14:07:38 +0200

    Removed showfirstcolumn, etc from PdfTexportTable sample

--- EPPlus9.WebSampleMvc.NetCore/Models/EPPlus9/PdfExportTableModel.cs
+++ EPPlus9.WebSampleMvc.NetCore/Models/EPPlus9/PdfExportTableModel.cs
@@ -63,10 +63,12 @@ namespace EPPlus9.WebSampleMvc.NetCore.Models.EPPlus9
             table.Columns[3].CalculatedColumnFormula = $"{table.Name}[[#This Row],[Population]]/{table.Name}[[#This Row],[Area (km²)]]";
             table.Columns[3].Name = "Density";
             table.Columns[3].TotalsRowFunction = RowFunctions.Average;
-            table.ShowFirstColumn = showFirstColumn;
-            table.ShowLastColumn = showLastColumn;
-            table.ShowColumnStripes = showColumnStripes;
-            table.ShowRowStripes = showRowStripes;
+            
+            // uncomment this to enable the show functionality
+            //table.ShowFirstColumn = showFirstColumn;
+            //table.ShowLastColumn = showLastColumn;
+            //table.ShowColumnStripes = showColumnStripes;
+            //table.ShowRowStripes = showRowStripes;
             sheet.Calculate();
 
             //// format the header
